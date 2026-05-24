fdb44083ff62fc932f4a9432a7ad50d7855ab515
Kaimune <kaimuart@gmail.com>
2026-05-24 14:00:16 +1000

    UI update 1

---
+++ Boss Jam Project/Assets/Art/VFX/VFX scripts/BlinkText.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using TMPro;
+
+public class BlinkText : MonoBehaviour
+{
+    public TextMeshProUGUI textObject;
+
+    public float speed = 2f;
+    public float minAlpha = 0.2f;
+    public float maxAlpha = 1f;
+
+    void Update()
+    {
+        Color color = textObject.color;
+
+        color.a = Mathf.Lerp(
+            minAlpha,
+            maxAlpha,
+            (Mathf.Sin(Time.time * speed) + 1f) / 2f
+        );
+
+        textObject.color = color;
+    }
+}
